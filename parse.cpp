a048ddb61f7b34f991fee6898942dae222d5e607
damion anderson <daander@clemson.edu>
2019-09-18 12:42:18 -0400

    just cleaning up code and pushing comments, now I need to finish cleaning the parse

--- parse.cpp
+++ parse.cpp
@@ -10,7 +10,9 @@ int main()
     while (file >> input)
     {
         // This portion will continue to scan until it finds "Athlete". Once it has found "athlete"
-        //
+        // it will start a separate while loop where it will continue to write to the individual 
+        // player's file. You WILL need to create a players directory. I am unsure if it will automatically
+        // create it for you
         if(input == "#" && file >> input && input == "Athlete:")
         {
             file >> input; // grab athlete name
@@ -23,7 +25,6 @@ int main()
             while(file >> input && input != "#")
             {   
                 output << input << " " << std::endl;
-
             }
         }
          
